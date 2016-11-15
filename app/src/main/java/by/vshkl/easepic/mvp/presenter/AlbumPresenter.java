ac5ed39e3b305776530999f855ab3294b1ab2a6b
Pavel Vashkel <jgugn1@gmail.com>
2016-11-15 16:32:06 +0300

    Album presenter added

---
+++ app/src/main/java/by/vshkl/easepic/mvp/presenter/AlbumPresenter.java
@@ -0,0 +1,72 @@
+package by.vshkl.easepic.mvp.presenter;
+
+import android.content.Context;
+
+import com.arellomobile.mvp.InjectViewState;
+import com.arellomobile.mvp.MvpPresenter;
+
+import java.util.List;
+
+import by.vshkl.easepic.mvp.model.Album;
+import by.vshkl.easepic.mvp.model.Picture;
+import by.vshkl.easepic.mvp.view.AlbumView;
+import by.vshkl.easepic.repository.LocalRepository;
+import by.vshkl.easepic.repository.Repository;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+
+@InjectViewState
+public class AlbumPresenter extends MvpPresenter<AlbumView> {
+
+    private Disposable disposable;
+    private Repository repository;
+    private Album.StorageType storageType;
+    private String albumId;
+
+    public void onStart(Context context) {
+        repository = new LocalRepository(context);
+        getViewState().showProgress();
+        getPictures();
+    }
+
+    public void onStop() {
+        if (disposable != null && !disposable.isDisposed()) {
+            disposable.dispose();
+        }
+    }
+
+    public void getPictures() {
+        System.out.println(storageType);
+        System.out.println(albumId);
+        disposable = repository.getPictures(storageType, albumId)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .onErrorReturn(new Function<Throwable, List<Picture>>() {
+                    @Override
+                    public List<Picture> apply(Throwable throwable) throws Exception {
+                        throwable.printStackTrace();
+                        getViewState().hideProgress();
+                        getViewState().showError(throwable.getMessage());
+                        return null;
+                    }
+                })
+                .subscribe(new Consumer<List<Picture>>() {
+                    @Override
+                    public void accept(List<Picture> pictureList) throws Exception {
+                        getViewState().hideProgress();
+                        getViewState().showPictures(pictureList);
+                    }
+                });
+    }
+
+    public void setStorageType(Album.StorageType storageType) {
+        this.storageType = storageType;
+    }
+
+    public void setAlbumId(String albumId) {
+        this.albumId = albumId;
+    }
+}
