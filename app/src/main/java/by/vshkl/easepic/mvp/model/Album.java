c88a471f30d53c7595f4b98e316fe05f21f7bb19
Pavel Vashkel <jgugn1@gmail.com>
2016-11-15 10:44:07 +0300

    Album model added

---
+++ app/src/main/java/by/vshkl/easepic/mvp/model/Album.java
@@ -0,0 +1,51 @@
+package by.vshkl.easepic.mvp.model;
+
+public class Album {
+
+    private String bucketId;
+    private String bucketName;
+
+    public Album() {
+    }
+
+    public String getBucketId() {
+        return bucketId;
+    }
+
+    public void setBucketId(String bucketId) {
+        this.bucketId = bucketId;
+    }
+
+    public String getBucketName() {
+        return bucketName;
+    }
+
+    public void setBucketName(String bucketName) {
+        this.bucketName = bucketName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Album album = (Album) o;
+
+        return bucketId.equals(album.bucketId);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return bucketId.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Album{");
+        sb.append("bucketId='").append(bucketId).append('\'');
+        sb.append(", bucketName='").append(bucketName).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
