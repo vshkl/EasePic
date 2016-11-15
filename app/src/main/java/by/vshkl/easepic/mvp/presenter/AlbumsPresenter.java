647e7e50034db4feac25998bad4331c59c9bf9e1
Pavel Vashkel <jgugn1@gmail.com>
2016-11-15 10:45:15 +0300

    AlbumsPresenter added

---
+++ app/src/main/java/by/vshkl/easepic/mvp/presenter/AlbumsPresenter.java
@@ -0,0 +1,59 @@
+package by.vshkl.easepic.mvp.presenter;
+
+import android.content.Context;
+
+import com.arellomobile.mvp.InjectViewState;
+import com.arellomobile.mvp.MvpPresenter;
+
+import java.util.List;
+
+import by.vshkl.easepic.mvp.model.Album;
+import by.vshkl.easepic.mvp.view.AlbumsView;
+import by.vshkl.easepic.repository.LocalRepository;
+import by.vshkl.easepic.repository.Repository;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+
+@InjectViewState
+public class AlbumsPresenter extends MvpPresenter<AlbumsView> {
+
+    private Disposable disposable;
+    private Repository repository;
+
+    public void onStart(Context context) {
+        repository = new LocalRepository(context);
+        getViewState().showProgress();
+        getLibrary();
+    }
+
+    public void onStop() {
+        if (disposable != null && !disposable.isDisposed()) {
+            disposable.dispose();
+        }
+    }
+
+    public void getLibrary() {
+        disposable = repository.getAlbums()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .onErrorReturn(new Function<Throwable, List<Album>>() {
+                    @Override
+                    public List<Album> apply(Throwable throwable) throws Exception {
+                        throwable.printStackTrace();
+                        getViewState().hideProgress();
+                        getViewState().showError(throwable.getMessage());
+                        return null;
+                    }
+                })
+                .subscribe(new Consumer<List<Album>>() {
+                    @Override
+                    public void accept(List<Album> albums) throws Exception {
+                        getViewState().hideProgress();
+                        getViewState().showAlbums(albums);
+                    }
+                });
+    }
+}
