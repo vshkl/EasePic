a557259f18207e1f85c9ed6781414985b9c392d0
Pavel Vashkel <jgugn1@gmail.com>
2016-11-17 18:59:05 +0300

    Create activity for ViewPager

---
+++ app/src/main/java/by/vshkl/easepic/ui/activity/PicturesPagerActivity.java
@@ -0,0 +1,62 @@
+package by.vshkl.easepic.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.arellomobile.mvp.MvpAppCompatActivity;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import by.vshkl.easepic.R;
+import by.vshkl.easepic.mvp.model.Picture;
+import by.vshkl.easepic.ui.adapter.PicturesPagerAdapter;
+
+public class PicturesPagerActivity extends MvpAppCompatActivity {
+
+    public static final String EXTRA_POSITION = "EXTRA_POSITION";
+    public static final String EXTRA_PICTURE_LIST = "EXTRA_PICTURE_LIST";
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.vp_pictures)
+    ViewPager vpPictures;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pictures_pager);
+        ButterKnife.bind(PicturesPagerActivity.this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            int position = intent.getIntExtra(EXTRA_POSITION, -1);
+            List<Picture> pictureList = intent.getParcelableArrayListExtra(EXTRA_PICTURE_LIST);
+
+            PicturesPagerAdapter adapter = new PicturesPagerAdapter(getSupportFragmentManager());
+            adapter.setPictureList(pictureList);
+
+            vpPictures.setAdapter(adapter);
+            vpPictures.setCurrentItem(position);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
