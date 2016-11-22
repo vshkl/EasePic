8977286e45f54940bedbfd0429353c13ae53e200
Pavel Vashkel <jgugn1@gmail.com>
2016-11-22 18:30:08 +0300

    Add method to get picture info to presenter

--- app/src/main/java/by/vshkl/easepic/mvp/presenter/PicturesPagerPresenter.java
+++ app/src/main/java/by/vshkl/easepic/mvp/presenter/PicturesPagerPresenter.java
@@ -8,9 +8,11 @@ import com.arellomobile.mvp.MvpPresenter;
 import java.util.List;
 
 import by.vshkl.easepic.mvp.model.Picture;
+import by.vshkl.easepic.mvp.model.PictureInfo;
 import by.vshkl.easepic.mvp.view.PicturesPagerView;
 import by.vshkl.easepic.repository.LocalRepository;
 import by.vshkl.easepic.repository.Repository;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
@@ -20,6 +22,7 @@ public class PicturesPagerPresenter extends MvpPresenter<PicturesPagerView> {
 
     private String picturesRootPath;
     private String pictureFullPath;
+    private String pictureId;
 
     public void getPictures(Context context) {
         Repository repository = new LocalRepository(context);
@@ -48,11 +51,38 @@ public class PicturesPagerPresenter extends MvpPresenter<PicturesPagerView> {
                 });
     }
 
+    public void getPictureInfo(Context context) {
+        Repository repository = new LocalRepository(context);
+
+        repository.getPictureInfo(pictureId, pictureFullPath)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .onErrorReturn(new Function<Throwable, PictureInfo>() {
+                    @Override
+                    public PictureInfo apply(Throwable throwable) throws Exception {
+                        throwable.printStackTrace();
+                        return null;
+                    }
+                })
+                .subscribe(new Consumer<PictureInfo>() {
+                    @Override
+                    public void accept(PictureInfo pictureInfo) throws Exception {
+                        if (pictureInfo != null) {
+                            getViewState().showPictureInfo(pictureInfo);
+                        }
+                    }
+                });
+    }
+
     public void setPicturesRootPath(String picturesRootPath) {
         this.picturesRootPath = picturesRootPath;
     }
 
     public void setPictureFullPath(String pictureFullPath) {
         this.pictureFullPath = pictureFullPath;
+    }
+
+    public void setPictureId(String pictureId) {
+        this.pictureId = pictureId;
     }
 }
