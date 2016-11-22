dff2da40534e9294842787ae94b6d7654bcb8aa7
Pavel Vashkel <jgugn1@gmail.com>
2016-11-22 13:05:07 +0300

    Create MarqueeTextView

---
+++ app/src/main/java/by/vshkl/easepic/ui/view/MarqueeTextView.java
@@ -0,0 +1,47 @@
+package by.vshkl.easepic.ui.view;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Rect;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class MarqueeTextView extends TextView {
+
+    public MarqueeTextView(Context context) {
+        super(context);
+    }
+
+    public MarqueeTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MarqueeTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public MarqueeTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+        if (focused) {
+            super.onFocusChanged(focused, direction, previouslyFocusedRect);
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+        if (hasWindowFocus) {
+            super.onWindowFocusChanged(hasWindowFocus);
+        }
+    }
+
+    @Override
+    public boolean isFocused() {
+        return true;
+    }
+}
